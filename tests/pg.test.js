7fac31e6edfc9bf1f8e4f79dfdad59eb3e1cc1b4
Talha76 <mushfiqurrahman845@gmail.com>
2023-03-31 22:47:19 +0600

    Removed try catch from test files, because node already handles the errors themselves

--- tests/pg.test.js
+++ tests/pg.test.js
@@ -3,16 +3,11 @@ const { pool, client } = require('../config/database.config');
 describe('PostgreSQL integration tests', () => {
   test('client should connect to database', async () => {
     const results = [];
-    try {
-      await client.connect();
-      const query = 'SELECT NOW()';
-      const res = await client.query(query);
-      res.rows.forEach(row => results.push(row));
-    } catch (err) {
-      console.error(err);
-    } finally {
-      await client.end();
-    }
+    await client.connect();
+    const query = 'SELECT NOW()';
+    const res = await client.query(query);
+    res.rows.forEach(row => results.push(row));
+    await client.end();
 
     expect(results.length).toBe(1);
     expect(results[0].now).toBeDefined();
@@ -21,16 +16,11 @@ describe('PostgreSQL integration tests', () => {
 
   test('pool should connect to database', async () => {
     const results = [];
-    try {
-      const clnt = await pool.connect();
-      const res = await clnt.query('SELECT NOW()');
-      res.rows.forEach(row => results.push(row));
-      await clnt.release();
-    } catch (err) {
-      console.error(err);
-    } finally {
-      await pool.end();
-    }
+    const clnt = await pool.connect();
+    const res = await clnt.query('SELECT NOW()');
+    res.rows.forEach(row => results.push(row));
+    await clnt.release();
+    await pool.end();
 
     expect(results.length).toBe(1);
     expect(results[0].now).toBeDefined();
--- tests/studentModel.test.js
+++ tests/studentModel.test.js
@@ -1,33 +1,23 @@
 const studentModel = require('../models/users/student.model');
 
 describe('User model tests', () => {
-  afterAll(() => {
-    try {
-      studentModel.destroy();
-    } catch (err) {
-      console.error(err);
-    }
-  });
+  afterAll(() => studentModel.destroy() );
 
   test('User should get proper user fields', async () => {
-    try {
-      const user = await studentModel.getStudentById(40);
-      expect(user.id).toBeDefined();
-      expect(user.name).toBeDefined();
-      expect(user.department).toBeDefined();
-      expect(user.gender).toBeDefined();
-      expect(user.email).toBeDefined();
-      expect(user.address).toBeDefined();
-      expect(user.guardianName).toBeDefined();
-      expect(user.guardianContact).toBeDefined();
-      expect(user.roomNo).toBeDefined();
-      expect(user.hallBlock).toBeDefined();
-      expect(user.country).toBeDefined();
-      expect(user.contactNo).toBeDefined();
-      expect(user.password).toBeDefined();
-    } catch (err) {
-      console.error(err);
-    }
+    const user = await studentModel.getStudentById(40);
+    expect(user.id).toBeDefined();
+    expect(user.name).toBeDefined();
+    expect(user.department).toBeDefined();
+    expect(user.gender).toBeDefined();
+    expect(user.email).toBeDefined();
+    expect(user.address).toBeDefined();
+    expect(user.guardianName).toBeDefined();
+    expect(user.guardianContact).toBeDefined();
+    expect(user.roomNo).toBeDefined();
+    expect(user.hallBlock).toBeDefined();
+    expect(user.country).toBeDefined();
+    expect(user.contactNo).toBeDefined();
+    expect(user.password).toBeDefined();
   });
 
 });
