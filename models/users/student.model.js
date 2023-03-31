bd6b96e8f7f10d6c7a2ede4731ab9d10b9af9c9f
Talha76 <mushfiqurrahman845@gmail.com>
2023-03-31 23:04:45 +0600

    Changed all functions to arrow functions

--- models/users/student.model.js
+++ models/users/student.model.js
@@ -1,6 +1,6 @@
 const { pool } = require('../../config/database.config');
 
-async function getStudentById(id) {
+const getStudentById = async (id) => {
   const query = `SELECT "students".*, "studentAuth".password `
               + `FROM "students" LEFT OUTER JOIN "studentAuth" ON "students"."id" = "studentAuth"."studentId" `
               + `WHERE "students"."id" = ${id}`;
@@ -26,7 +26,7 @@ async function getStudentById(id) {
   });
 }
 
-async function saveLeaveInfo(id, info) {
+const saveLeaveInfo = async (id, info) => {
   const query = `INSERT INTO "leaveInfo" ("studentId", "placeOfVisit", "purposeOfVisit", "departureDate", "arrivalDate", "contact", "guardianContact") `
               + `VALUES (${id}, '${info.placeOfVisit}', '${info.purposeOfVisit}', '${info.departureDate}', '${info.arrivalDate}', `
               + `'${info.studentContact}', '${info.contactPersonContact}')`;
@@ -46,46 +46,10 @@ async function saveLeaveInfo(id, info) {
   });
 }
 
-async function deleteLeaveInfo(leaveId) {
-  const query = `DELETE FROM "leaveInfo" WHERE "id" = ${leaveId}`;
-  return new Promise((resolve, reject) => {
-    pool.connect()
-      .then((client) => {
-        client.query(query, (err, res) => {
-          client.release();
-          if (err) {
-            reject(err);
-          } else {
-            resolve(res);
-          }
-        });
-      })
-      .catch((err) => reject(err));
-  });
-}
-
-async function saveLateInfo(id, info) {
+const saveLateInfo = async (id, info) => {
   const query = `INSERT INTO "lateInfo" ("studentId", "placeOfVisit", "reason", "departureTime", "arrivalTime", "contact", "accompanyingPersonContact") `
               + `VALUES (${id}, '${info.placeOfVisit}', '${info.lateReason}', '${info.departureTime}', '${info.arrivalTime}', `
               + `'${info.studentContact}', '${info.accompanyingPersonContact}')`;
-  return new Promise((resolve, reject) => {
-    pool.connect()
-      .then((client) => {
-        client.query(query, (err, res) => {
-          client.release();
-          if (err) {
-            reject(err);
-          } else {
-            resolve(res);
-          }
-        });
-      })
-      .catch((err) => reject(err));
-  });
-}
-
-async function deleteLateInfo(lateId) {
-  const query = `DELETE FROM "lateInfo" WHERE "id" = ${lateId}`;
   return new Promise((resolve, reject) => {
     pool.connect()
       .then((client) => {
@@ -107,8 +71,6 @@ async function destroy() { pool.end(); }
 module.exports = {
   getStudentById,
   saveLeaveInfo,
-  deleteLeaveInfo,
   saveLateInfo,
-  deleteLateInfo,
-  destroy,
+  destroy
 };
