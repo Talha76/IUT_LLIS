1fe1428fa99f131f741469b90fe64b440a9050a3
Talha76 <mushfiqurrahman845@gmail.com>
2023-05-19 10:03:51 +0600

    Updated postgres integration test

--- tests/pg.test.js
+++ tests/pg.test.js
@@ -1,25 +1,12 @@
 const { pool, client } = require('../config/database.config');
 
 describe('PostgreSQL integration tests', () => {
-  test('client should connect to database', async () => {
-    const results = [];
-    await client.connect();
-    const query = 'SELECT NOW()';
-    const res = await client.query(query);
-    res.rows.forEach(row => results.push(row));
-    await client.end();
-
-    expect(results.length).toBe(1);
-    expect(results[0].now).toBeDefined();
-    expect(results[0].then).toBeUndefined();
-  });
-
   test('pool should connect to database', async () => {
     const results = [];
     const clnt = await pool.connect();
     const res = await clnt.query('SELECT NOW()');
     res.rows.forEach(row => results.push(row));
-    await clnt.release();
+    clnt.release();
     await pool.end();
 
     expect(results.length).toBe(1);
