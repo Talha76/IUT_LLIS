f1660638bbc4faba30bdb7c6d592b52b4e4d3e78
Talha76 <mushfiqurrahman845@gmail.com>
2023-06-07 23:05:07 +0600

    Updated admin model with poolExecute

--- models/hallAdmin.model.js
+++ models/hallAdmin.model.js
@@ -1,57 +1,28 @@
-const { pool } = require('../config/database.config');
+const { pool, poolExecute } = require('../config/database.config');
 
 const approveLeave = async (leaveId, role) => {
   const query = `UPDATE "leaveInfo" SET "${role}Status" = 'approved' `
               + `WHERE "leaveId" = ${leaveId}`;
-  
-  return new Promise((resolve, reject) => {
-    pool.connect()
-      .then((client) => {
-        client.query(query)
-          .then((result) => resolve(null))
-          .catch((err) => reject(err))
-          .finally(() => client.release());
-      })
-      .catch((err) => reject(err));
-  });
+
+  await poolExecute(query);
 }
 
 const rejectLeave = async (leaveId, role) => {
   const query = `UPDATE "leaveInfo" SET "${role}Status" = 'rejected' `
               + `WHERE "leaveId" = ${leaveId}`;
   
-  return new Promise((resolve, reject) => {
-    pool.connect()
-      .then((client) => {
-        client.query(query)
-          .then((result) => resolve(null))
-          .catch((err) => reject(err))
-          .finally(() => client.release());
-      })
-      .catch((err) => reject(err));
-  });
+  await poolExecute(query);
 }
 
 const getAdminById = async (id) => {
   const query = `SELECT "id", "name", "email", "password", "adminType" as "role" FROM "adminAuth"`
               + `WHERE "id" = ${id}`;
   
-  return new Promise((resolve, reject) => {
-    pool.connect()
-      .then((client) => {
-        client.query(query)
-          .then((res) => {
-            if (res.rowCount < 1) {
-              resolve(null);
-            } else {
-              resolve(res.rows[0]);
-            }
-          })
-          .catch((err) => reject(err))
-          .finally(() => client.release());
-      })
-      .catch((err) => reject(err));
-  });
+  const result = await poolExecute(query);
+  if (result.rowCount < 1) {
+    return null;
+  }
+  return result[0];
 }
 
 const destroy = async() => pool.end();
