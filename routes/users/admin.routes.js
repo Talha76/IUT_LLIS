eda4963b02420d352c1ab17e9a4b112b3489acb2
Talha76 <mushfiqurrahman845@gmail.com>
2023-06-07 22:52:59 +0600

    Added admin forgot-password get

--- routes/users/admin.routes.js
+++ routes/users/admin.routes.js
@@ -6,13 +6,28 @@ const { ensureAdminAuth, ensureAdminNotAuth } = require('../../middlewares/auth.
 
 router.get ('/', ensureAdminNotAuth, adminModules.getAdminIndex);
 router.post('/', ensureAdminNotAuth, middleware.indexPassportAuth, adminModules.postAdminIndex);
-router.get('/dashboard', ensureAdminAuth, middleware.isAdmin, adminModules.getAdminDashboard);
-router.get('/generate-leave-report', ensureAdminAuth, middleware.isAdmin, adminModules.getLeaveReport);
+
+router.get('/forgot-password', ensureAdminNotAuth, (req, res) => {
+  res.render('users/forgot-password-admin', {
+    error: req.flash('error'),
+    message: req.flash('message')
+  })
+})
+
 router.get('/logout', ensureAdminAuth, adminModules.getLogout);
+
+router.get('/dashboard', ensureAdminAuth, middleware.isAdmin, adminModules.getAdminDashboard);
+
+router.get('/generate-leave-report', ensureAdminAuth, middleware.isAdmin, adminModules.getLeaveReport);
+
 router.get('/details', ensureAdminAuth, middleware.isAdmin, adminModules.getDetails);
+
 router.get('/history/details', ensureAdminAuth, middleware.isAdmin, adminModules.getHistoryDetails);
+
 router.get('/generate-late-report', ensureAdminAuth, middleware.isAdmin, adminModules.getLateReport);
+
 router.get('/approve', ensureAdminAuth, middleware.isAdmin, adminModules.getApprove);
+
 router.get('/reject', ensureAdminAuth, middleware.isAdmin, adminModules.getReject);
 
 module.exports = router;
